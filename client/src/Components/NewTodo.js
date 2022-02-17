e17a244cb1c578750bdce9871288fe922f943f30
Lauren Hess <laurenbethhess@gmail.com>
2022-02-17 13:10:39 -0500

    changed proxy back to heroku

--- client/src/Components/NewTodo.js
+++ client/src/Components/NewTodo.js
@@ -19,7 +19,7 @@ function NewTodo({ onAddTodo, user }) {
     function handleSubmit(e) {
       e.preventDefault()
   
-      fetch("https://the-plan-for-now.herokuapp.com/todos/", {
+      fetch("/todos", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
